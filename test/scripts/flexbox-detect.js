c644493d95d51de9bd4e37a7203cfc88284e5e71
serrintine <serrintine@gmail.com>
2014-09-02 03:32:02 -0400

    cleanup

--- test/scripts/flexbox-detect.js
+++
@@ -1,8 +0,0 @@
-;(function(body, div) {
-  if (body.style.flex == null && body.style.webkitFlex == null) {
-    div = document.createElement("div")
-    div.className = "Error"
-    div.innerHTML = "Your browser does not support Flexbox. Parts of this site may not appear as expected."
-    body.insertBefore(div, body.firstChild)
-  }
-}(document.body))
